aec692c4fcf43d3b5ce68f3789718e241bbf17ba
SHEMNDARONGUGI <ndaroshem@gmal.com>
2025-10-21 15:13:11 +0300

    creating mymodel, creating templates and setting up my views and paths

--- myshop/myshopapp/models.py
+++ myshop/myshopapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Product(models.Model):
+    image = models.ImageField(upload_to='uploads/product/')
+    title = models.CharField(default='', max_length=255, blank=False)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+
+    def __str__(self):
+        return self.title
--- myshop/myshopapp/views.py
+++ myshop/myshopapp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Product
 
 # Create your views here.
+def my_product(request):
+    if request.method == 'POST':
+        myimage = Product(
+            image = request.FILES.get('image'),
+            title = request.POST['title'],
+            price = request.POST['price']
+        )
+        myimage.save()
+        return redirect ('shop')
+    
+    else:
+        return render(request, 'index.html')
